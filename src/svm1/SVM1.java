0783e6217c5b47901ea6cdadf1816cbb24d49d26
=csgsajeewa <csgsajeewa@gmail.com>
2014-10-30 12:08:35 +0530

    1st commit

---
+++ src/svm1/SVM1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package svm1;
+
+import libsvm.svm_model;
+
+/**
+ *
+ * @author hp
+ */
+public class SVM1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SVMTest svmt=new SVMTest();
+        svm_model model=svmt.svmTrain();
+        double[] features={1,0,550};
+        double v=svmt.predict(features, model);
+        System.out.println(v);
+        svmt.evaluate(features, model);
+    }
+}

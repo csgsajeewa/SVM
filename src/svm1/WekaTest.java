8978778eca705a6091772e4a2550f144a8e1a2b4
=csgsajeewa <csgsajeewa@gmail.com>
2014-11-09 06:57:09 +0530

    add multi class classification support

--- src/svm1/WekaTest.java
+++ src/svm1/WekaTest.java
@@ -6,9 +6,11 @@ package svm1;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.ObjectOutputStream;
 import java.util.Random;
 import weka.attributeSelection.CfsSubsetEval;
 import weka.attributeSelection.GreedyStepwise;
@@ -20,9 +22,7 @@ import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SelectedTag;
-import weka.core.SparseInstance;
 import weka.core.Tag;
-import weka.core.Utils;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.converters.TextDirectoryLoader;
 import weka.core.stemmers.SnowballStemmer;
@@ -92,6 +92,11 @@ public class WekaTest {
 
     }
 
+    /**
+     *
+     * @throws IOException
+     * @throws Exception
+     */
     public void loadFromTextFiles() throws IOException, Exception {
 
         SnowballStemmer stemmer = new SnowballStemmer();
@@ -99,7 +104,7 @@ public class WekaTest {
         StringToWordVector filter = new StringToWordVector();
         filter.setLowerCaseTokens(true);
         filter.setOutputWordCounts(true);
-        //     filter.setTFTransform(true);
+        filter.setTFTransform(true);
         filter.setIDFTransform(true);
         filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWords.txt"));
         filter.setStemmer(stemmer);
@@ -120,7 +125,7 @@ public class WekaTest {
         //System.out.println(dataFiltered.classIndex());
 
         LibSVM svm = new LibSVM();
-        String svmOptions = "-S 1 -K 3";
+        String svmOptions = "-S 0 -K 2 -C 1 -G 0.0038498";
         svm.setOptions(weka.core.Utils.splitOptions(svmOptions));
         //divide dataFiltered into training test and test set
         Instances class1Data = new Instances(dataFiltered, 0, dataFiltered.numInstances() / 2);
@@ -176,6 +181,10 @@ public class WekaTest {
         System.out.println(eTest.weightedAreaUnderROC());
     }
 
+    /**
+     *
+     * @throws Exception
+     */
     public void testCrossValidataion() throws Exception {
 
         //set tokenizer - we can specify n-grams for classification
@@ -192,9 +201,9 @@ public class WekaTest {
         StringToWordVector filter = new StringToWordVector();
         filter.setLowerCaseTokens(true);
         filter.setOutputWordCounts(true);
-//        filter.setTFTransform(true);
-        filter.setIDFTransform(true);
-        filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWordsR1.txt"));
+//      filter.setTFTransform(true);
+//      filter.setIDFTransform(true);
+//      filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWordsR1.txt"));
         filter.setTokenizer(tokenizer);
         filter.setStemmer(stemmer);
         System.out.println("Stemmer Name- " + filter.getStemmer());
@@ -208,13 +217,13 @@ public class WekaTest {
         // apply the StringToWordVector filter
         filter.setInputFormat(data);
         Instances dataFiltered = Filter.useFilter(data, filter);
-        System.out.println("Number of Attributes- " + dataFiltered.numAttributes());
+        System.out.println("Number of Attributes after stop words removal- " + dataFiltered.numAttributes());
         System.out.println("\n\nFiltered data:\n\n" + dataFiltered);
 
         //initialize the model and set SVM type and kernal type
         LibSVM svm = new LibSVM();
         //-S 1 -K 3 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C 1.0 -E 0.0010 -P 0.1 Best G 0.001953125 for C=32578
-        String svmOptions = "-S 0 -K 2 -C 1 -G 0.001953125";
+        String svmOptions = "-S 0 -K 2 -C 32578 -G 0.0038498";
         svm.setOptions(weka.core.Utils.splitOptions(svmOptions));
         System.out.println("&&&&&&&&" + svm.getSVMType() + svm.getKernelType());//1,3 best result 81%
 
@@ -222,14 +231,30 @@ public class WekaTest {
         //dataFiltered=featureSelection(dataFiltered);
         //System.out.println("Selected Features"+dataFiltered);
 
-        gridSearch(svm, dataFiltered);
-      //  perform cross vlaidation
-//        Evaluation evaluation = new Evaluation(dataFiltered);
-//        evaluation.crossValidateModel(svm, dataFiltered, 2, new Random(1));
-//        System.out.println(evaluation.toSummaryString());
-//        System.out.println(evaluation.weightedAreaUnderROC());
+        //  gridSearch(svm, dataFiltered);
+        //    perform cross vlaidation
+        Evaluation evaluation = new Evaluation(dataFiltered);
+        evaluation.crossValidateModel(svm, dataFiltered, 2, new Random(1));
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.weightedAreaUnderROC());
+
+        //get test instances and perform predictions
+        Instances testData = createTestInstances();
+        Instances testDataFiltered = Filter.useFilter(testData, filter);
+        svm.buildClassifier(dataFiltered);
+
+        for (int i = 0; i < testDataFiltered.numInstances(); i++) {
+            System.out.println("\n\nTEST Filtered data:\n\n" + testDataFiltered.instance(i));
+            double[] results = svm.distributionForInstance(testDataFiltered.instance(i));
+            System.out.println(results[0] + " " + results[1]);
+        }
+        // saveClassifier(svm);
     }
 
+    /**
+     *
+     * @throws Exception
+     */
     public void testFilter() throws Exception {
         StringToWordVector filter = new StringToWordVector();
         String[] options = {"-C", "-T", "-I", "-S", "-L"};
@@ -237,16 +262,22 @@ public class WekaTest {
         System.out.println(filter.getLowerCaseTokens());
     }
 
+    /**
+     *
+     * @param svm
+     * @param dataFiltered
+     * @throws Exception
+     */
     public void gridSearch(LibSVM svm, Instances dataFiltered) throws Exception {
         GridSearch gs = new GridSearch();
         gs.setClassifier(svm);
         gs.setFilter(new AllFilter());
 
-        
+
 
         gs.setXProperty("classifier.cost");
-        gs.setXMin(-5);
-        gs.setXMax(15);
+        gs.setXMin(3);
+        gs.setXMax(25);
         gs.setXStep(2);
         gs.setXBase(2);
         gs.setXExpression("pow(BASE,I)");
@@ -254,7 +285,7 @@ public class WekaTest {
         gs.setYProperty("classifier.gamma");
         gs.setYMin(-8.5);
         gs.setYMax(-8);
-        gs.setYStep(0.05);
+        gs.setYStep(0.001);
         gs.setYBase(2);
         gs.setYExpression("pow(BASE,I)");
         //-y-property classifier.kernel.gamma -y-min -5.0 -y-max 2.0 -y-step 1.0 -y-base 10.0 -y-expression pow(BASE,I) -filter weka.filters.AllFilter -x-property classifier.nu -x-min 0.01 -x-max 1.0 -x-step 10.0 -x-base 10.0 -x-expression I -sample-size 100.0 -traversal COLUMN-WISE -log-file "C:\Program Files\Weka-3-6" -S 1 -W weka.classifiers.functions.LibSVM -- -S 2 -K 2 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C 1.0 -E 0.0010 -P 0.1
@@ -266,7 +297,7 @@ public class WekaTest {
         int EVALUATION_RAE = 4;
         int EVALUATION_COMBINED = 5;
         int EVALUATION_ACC = 6;
-        int EVALUATION_KAPPA = 7;    
+        int EVALUATION_KAPPA = 7;
         Tag[] TAGS_EVALUATION = {
             new Tag(EVALUATION_CC, "CC", "Correlation coefficient"),
             new Tag(EVALUATION_RMSE, "RMSE", "Root mean squared error"),
@@ -277,17 +308,23 @@ public class WekaTest {
             new Tag(EVALUATION_ACC, "ACC", "Accuracy"),
             new Tag(EVALUATION_KAPPA, "KAP", "Kappa")
         };
-        SelectedTag st=new SelectedTag(EVALUATION_ACC, TAGS_EVALUATION);
+        SelectedTag st = new SelectedTag(EVALUATION_ACC, TAGS_EVALUATION);
         System.out.println(st.getTags());
         gs.setEvaluation(st);
-        
-        gs.setDebug(true);  
-        
+
+        gs.setDebug(true);
+
         gs.buildClassifier(dataFiltered);
         System.out.println("Criteria " + gs.getEvaluation().getSelectedTag().getID());
         System.out.println("&&&&&&&&&&&&" + gs.getValues());
     }
 
+    /**
+     *
+     * @param data
+     * @return
+     * @throws Exception
+     */
     public Instances featureSelection(Instances data) throws Exception {
         System.out.println("Attribute Selection");
         AttributeSelection filter = new AttributeSelection();
@@ -299,5 +336,119 @@ public class WekaTest {
         filter.setInputFormat(data);
         Instances newData = Filter.useFilter(data, filter);
         return newData;
+    }
+
+    /**
+     *
+     * @return @throws FileNotFoundException
+     * @throws IOException
+     */
+    public Instances createTestInstances() throws FileNotFoundException, IOException {
+
+
+        FastVector attributeList = new FastVector(2);
+        Attribute a1 = new Attribute("text", (FastVector) null);
+
+        FastVector classVal = new FastVector();
+        classVal.addElement("crimes");
+        classVal.addElement("others");
+        Attribute c = new Attribute("@@class@@", classVal);
+
+        //add class attribute and news text
+        attributeList.addElement(a1);
+        attributeList.addElement(c);
+
+
+
+        Instances testData = new Instances("TestNews", attributeList, 0);
+        if (testData.classIndex() == -1) {
+            testData.setClassIndex(1);
+        }
+        //for each test instance
+        File newsDirectory = new File("C:\\Users\\hp\\Desktop\\SVM implementation\\TestNews");
+        BufferedReader newsArticleReader;
+        for (File fileEntry : newsDirectory.listFiles()) {
+            System.out.println(fileEntry.getPath());
+            newsArticleReader = new BufferedReader(new FileReader(fileEntry.getPath()));
+            String news = "";
+            String newsLine = "";
+            while ((newsLine = newsArticleReader.readLine()) != null) {
+                news = news.concat(newsLine);
+                news = news.concat(" ");
+            }
+            Instance inst = new Instance(testData.numAttributes());
+            inst.setValue(a1, news);
+
+            inst.setDataset(testData);
+            inst.setClassMissing();
+
+            System.out.println(inst);
+            testData.add(inst);
+        }
+        return testData;
+    }
+
+    private void saveClassifier(LibSVM svm) throws IOException {
+        //save instance
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("/some/where/j48.model"));
+        oos.writeObject(svm);
+        oos.flush();
+        oos.close();
+    }
+
+    public void testMultiClassClassification() throws Exception {
+        //set tokenizer - we can specify n-grams for classification
+        NGramTokenizer tokenizer = new NGramTokenizer();
+        tokenizer.setNGramMinSize(1);
+        tokenizer.setNGramMaxSize(1);
+        tokenizer.setDelimiters("\\W");
+
+        //set stemmer - set english stemmer
+        SnowballStemmer stemmer = new SnowballStemmer();
+        stemmer.setStemmer("english");
+
+        //create new filter for vector transformation
+        StringToWordVector filter = new StringToWordVector();
+        filter.setLowerCaseTokens(true);
+        filter.setOutputWordCounts(true);
+//      filter.setTFTransform(true);
+//      filter.setIDFTransform(true);
+//      filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWordsR1.txt"));
+        filter.setTokenizer(tokenizer);
+        filter.setStemmer(stemmer);
+        System.out.println("Stemmer Name- " + filter.getStemmer());
+
+        //import data from file
+        TextDirectoryLoader docLoader = new TextDirectoryLoader();
+        docLoader.setDirectory(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\CrimeAccidentsOthers"));
+        Instances data = docLoader.getDataSet();
+        System.out.println("\n\nImported data:\n\n" + data);
+
+        // apply the StringToWordVector filter
+        filter.setInputFormat(data);
+        Instances dataFiltered = Filter.useFilter(data, filter);
+        System.out.println("Number of Attributes after stop words removal- " + dataFiltered.numAttributes());
+        System.out.println("\n\nFiltered data:\n\n" + dataFiltered);
+
+        //initialize the model and set SVM type and kernal type
+        LibSVM svm = new LibSVM();
+        //-S 1 -K 3 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C 1.0 -E 0.0010 -P 0.1 Best G 0.001953125 for C=32578
+        String svmOptions = "-S 0 -K 2 -C 32578 -G 0.0038498";
+        svm.setOptions(weka.core.Utils.splitOptions(svmOptions));
+        System.out.println("&&&&&&&&" + svm.getSVMType() + svm.getKernelType());//1,3 best result 81%
+
+        //select most relevant features for classification
+        //dataFiltered=featureSelection(dataFiltered);
+        //System.out.println("Selected Features"+dataFiltered);
+
+        //  gridSearch(svm, dataFiltered);
+        //    perform cross vlaidation
+        Evaluation evaluation = new Evaluation(dataFiltered);
+        evaluation.crossValidateModel(svm, dataFiltered, 2, new Random(1));
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.weightedAreaUnderROC());
+
+       
+
     }
 }
